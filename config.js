4e75daf48827a5a6ad7911e01b0814382e7b1ce7
yeuemthuong <114825034+yeuemthuong@users.noreply.github.com>
2022-10-01 23:45:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
    titleWeb: "I Love You",
-    introTitle: 'Ngọc Uyên',
+    introTitle: 'Bé Thương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối mốt 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100044991774359' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/huutrong.l/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
